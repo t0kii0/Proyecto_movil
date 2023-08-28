bc1f4152c242cdbd4513285b0347ea5fd1a548b0
Manuus1 <106639853+Manuus1@users.noreply.github.com>
2023-08-28 17:14:33 -0400

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'IonicLogin',
+  appName: 'Duocapp',
   webDir: 'www',
   server: {
     androidScheme: 'https'
