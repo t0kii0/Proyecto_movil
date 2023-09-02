7cb9899d8336b93cf291ca4dc13070bed0b6668f
fel.martinezl@duocuc.cl <fel.martinezl@duocuc.cl>
2023-09-02 12:04:40 -0400

    se agrega pantalla home

--- src/app/recuperar/recuperar.page.spec.ts
+++ src/app/recuperar/recuperar.page.spec.ts
@@ -15,3 +15,7 @@ describe('RecuperarPage', () => {
     expect(component).toBeTruthy();
   });
 });
+
+function async(arg0: () => void): jasmine.ImplementationCallback {
+  throw new Error('Function not implemented.');
+}
