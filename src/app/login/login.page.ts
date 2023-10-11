ece2bc2fdc022ff331484ab5a17e18329cec5587
Manuus1 <106639853+Manuus1@users.noreply.github.com>
2023-10-11 17:01:01 -0300

    se agrega conexion con bd

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {
   FormGroup,
@@ -7,6 +7,24 @@ import {
   FormBuilder
 } from '@angular/forms';
 import { AlertController } from '@ionic/angular';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  apiUrl = 'https://staxsitmytxxwupentta.supabase.co/rest/v1/CONDUCTOR';
+
+  constructor(private http: HttpClient) {}
+
+  getData() {
+    const headers = new HttpHeaders({
+      'apiKey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0YXhzaXRteXR4eHd1cGVudHRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU3NzQ2NjksImV4cCI6MjAxMTM1MDY2OX0.zwia42h8na8h1p52H0L8B_-IkHAS-WooRAWP5nGlC2Q'
+    });
+
+    return this.http.get(this.apiUrl, { headers });
+  }
+}
 
 @Component({
   selector: 'app-login',
@@ -18,24 +36,33 @@ export class LoginPage implements OnInit {
   formularioLogin: FormGroup;
 
   usuarios = [
-    { nombre: 'Manuu', correo: 'manu@gmail.com', password: 'manu150',rol: 'conductor' },
-    { nombre: 'Angelo', correo: 'angelo@gmail.com', password: 'angelo150',rol: 'pasajero' },
+    { nombre: 'Manuu', correo: 'manu@gmail.com', password: 'manu150', rol: 'conductor' },
+    { nombre: 'Angelo', correo: 'angelo@gmail.com', password: 'angelo150', rol: 'pasajero' },
   ];
-  
-  constructor(public fb: FormBuilder,  
-    public alertController: AlertController, private router: Router){
 
+  constructor(public fb: FormBuilder,
+    public alertController: AlertController, private router: Router, private http: HttpClient, private ApiService: ApiService) {
     this.formularioLogin = this.fb.group({
-      'nombre' : new FormControl("",Validators.required),
-      'password' : new FormControl("",Validators.required)
-    })
-  }  
-  ngOnInit() {
+      'nombre': new FormControl("", Validators.required),
+      'password': new FormControl("", Validators.required)
+    });
   }
+
+  ngOnInit() {
+    this.ApiService.getData().subscribe(
+      (res) => {
+        console.log(res);
+      },
+      (error) => {
+        console.error('Error', error);
+      }
+    );
+  }
+
   async ingresar() {
     var f = this.formularioLogin.value;
-    const usuario = this.usuarios.find(u => u.nombre === f.nombre && u.password === f.password); 
-  
+    const usuario = this.usuarios.find(u => u.nombre === f.nombre && u.password === f.password);
+
     if (usuario) {
       console.log('Ingreso exitoso');
       this.router.navigate(['/home-usuario']);
