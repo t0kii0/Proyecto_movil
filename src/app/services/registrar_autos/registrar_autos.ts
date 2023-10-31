043554a60b38ef2f24b4a3c26221bd2d08814232
t0kii0 <ang.sepulvedao@duocuc.cl>
2023-10-31 01:12:50 -0300

    ultimos intentos

--- src/app/services/registrar_autos/registrar_autos.ts
+++ src/app/services/registrar_autos/registrar_autos.ts
@@ -6,12 +6,12 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
-export class RegistrosAuto {
+export class RegistrosAuto { 
     constructor(private _http: HttpClient) { }
 
     superbaseUrl = "https://staxsitmytxxwupentta.supabase.co/rest/v1/"
     supabaseHeaders = new HttpHeaders().set("apiKey", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0YXhzaXRteXR4eHd1cGVudHRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU3NzQ2NjksImV4cCI6MjAxMTM1MDY2OX0.zwia42h8na8h1p52H0L8B_-IkHAS-WooRAWP5nGlC2Q")
 
-    guardarAuto(registros: RegistrarAuto): Observable<string | any>{
-        return this._http.post<any>(this.superbaseUrl+'AUTO',registros,{headers: this.supabaseHeaders});
+    guardarAuto(registros1: RegistrarAuto): Observable<string | any>{
+        return this._http.post<any>(this.superbaseUrl+'AUTO',registros1,{headers: this.supabaseHeaders});
       }}
